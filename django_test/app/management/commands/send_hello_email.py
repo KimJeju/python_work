638b59687f9c039d6e49ced2542b086b3c3143b0
kimjeju <h.hwi9367@gmail.com>
2023-12-31 21:39:42 +0900

    장고 email 라이브러리로 이메일 보내보기

--- django_test/app/management/commands/send_hello_email.py
+++ django_test/app/management/commands/send_hello_email.py
@@ -5,15 +5,16 @@ from django.core.exceptions import ValidationError
 from django.core.mail import send_mail
 from django.core.management import BaseCommand, CommandError
 from django.core.validators import validate_email
+from django.template.loader import render_to_string
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parse):
-        parse.add_argument("receiver", narge="+", type=str, help="hwi9367@naver.com")
+    def add_arguments(self, parser):
+        parser.add_argument("receiver", nargs="+", type=str, help=">dz!®–|!õ—")
 
     def handle(self, *args, **options):
-        subject = "장고 이메일 발송"
-        content = "테스트 입니다."
+        subject = render_to_string("app/hello_email_subject.txt")
+        content = render_to_string("app/hello_email_content.txt", {"event_name": "파이썬"})
         sender_email = settings.DEFAULT_FROM_EMAIL
         receiver_email_list: List[str] = options["receiver"]
 
