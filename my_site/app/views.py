05e6027b7f5da1ce03b4f7fe127d3a88baa9d50c
kimjeju <h.hwi9367@gmail.com>
2024-01-18 23:02:33 +0900

    django-bootstrap 적용

--- my_site/app/views.py
+++ my_site/app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView
 
+from app.forms import PostForm
 from app.models import Post
 
 
@@ -8,11 +10,14 @@ from app.models import Post
 def index(request : HttpRequest) -> HttpResponse:
     qs = Post.objects.all()
     return render(request, "app/index.html",{
-        "post_list" : qs
+        "post_list":qs
     })
 
 def post_detail(requset : HttpRequest, pk : int) ->  HttpResponse:
         post = get_object_or_404(Post, pk=pk)
         return render(requset, "app/post_detail.html",{
-            "post" : post
+            "post":post
         })
+
+
+post_new = CreateView.as_view(model = Post, form_class = PostForm, success_url="/app/")
--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -22,5 +22,7 @@ from app import views as app_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/',app_views.index),
-    path('app/<int:pk>/', app_views.post_detail)
+    path('app/<int:pk>/', app_views.post_detail),
+    path('app/new', app_views.post_new)
 ]
+
