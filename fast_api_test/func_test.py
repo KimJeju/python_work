c8f17fa7dd804ffdb9eaf4abf8e3c210d1beb0c9
kimjeju <h.hwi9367@gmail.com>
2024-05-23 21:00:53 +0900

    분기별 범죄통계 데이터 세팅

--- fast_api_test/func_test.py
+++
@@ -1,9 +0,0 @@
-def get_full_name(first_name :str, last_name:str):
-    full_name = first_name.title() + last_name.title()
-    return full_name
-
-def get_name_with_age(name:str, age:int):
-    name_with_age = name + "은" + str(age) + "살"
-    return name_with_age
-
-print(get_full_name("John", "Smith"))
