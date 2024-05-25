f4750c69b8794111196c4d6aecb6a1107664386d
kimjeju <h.hwi9367@gmail.com>
2024-05-25 19:07:02 +0900

    test

--- crime_dash_board/crime_dash_board_server/branch_crime_app/branch_crime_util.py
+++ crime_dash_board/crime_dash_board_server/branch_crime_app/branch_crime_util.py
@@ -5,18 +5,15 @@ def get_default_crime_branch_data():
     
     category = ['average','main', 'sub']
     dict_data = {}
-    
     try:
         for i in (0,2):            
             csv_data = pd.read_csv(f'./templates/crime_data/branch/2024/category/{category[i]}/crime_report_branch_1_2024_{category[i]}_category.csv')
 
-            
             df = pd.DataFrame(csv_data)
             df = sort_crime_data(df)
                         
             dict_data[category[i]] = df
-        
-        return dict_data        
+        return csv_data        
     except Exception:
         print(FileNotFoundError)    
         
