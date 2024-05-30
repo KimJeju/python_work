c434d6587ade5f60dbe05deac696a3a3f6af9f0d
kimjeju <h.hwi9367@gmail.com>
2024-05-30 23:45:06 +0900

    데이터 재정의 및 에러 해결

--- crime_dash_board/crime_dash_board_server/branch_crime_app/branch_crime_util.py
+++ crime_dash_board/crime_dash_board_server/branch_crime_app/branch_crime_util.py
@@ -5,27 +5,35 @@ def get_default_crime_branch_data():
     category = ['average','main', 'sub']
     dict_data = {}
     try:
-        for i in range(len(category)):            
-            csv_data = pd.read_csv(f'./templates/crime_data/branch/2024/category/{category[i]}/crime_report_branch_1_2024_{category[i]}_category.csv')
-
+        for i in range(0,3):     
+            csv_data = pd.read_csv(f'./templates/crime_data/branch/2024/{category[i]}/crime_report_branch_1_2024_{category[i]}_crime_data.csv')
             df = pd.DataFrame(csv_data)
-                                    
+            df = sort_crime_data(df)
             dict_data[category[i]] = df
-        return dict_data        
+        return dict_data       
     except Exception:
         print(FileNotFoundError)    
         
 # 인자 값에 따른 데이터 불러오기 메서드
 def get_categorize_crime_branch(year:str, branch:int, category:str):
+    dict_data = {}
     try:
-        dict_data = {}
-    
-        csv_data = pd.read_csv(f'./templates/crime_data/branch/{year}/category/{category}/crime_report_branch_{branch}_2024_{category}_category.csv')
-    
+        csv_data = pd.read_csv(f'./templates/crime_data/branch/{year}/{category}/crime_report_branch_{branch}_{year}_{category}_crime_data.csv')
         df = pd.DataFrame(csv_data)
+        df = sort_crime_data(df)
         dict_data[f"{category} ({year} {branch}분기)"] = df
     
         return dict_data
     except Exception:
         print(FileNotFoundError)    
 
+# 데이터 정리 메서드
+def sort_crime_data(data : pd.DataFrame):
+    try:
+        data = data.transpose()
+        data.rename(columns=data.iloc[0], inplace=True)
+        data = data.drop(data.index[0])
+                        
+        return data
+    except Exception:
+        print(FileNotFoundError)    
