04dbdedeed3c17031649a20edcd4adf30f0a2af6
kimjeju <h.hwi9367@gmail.com>
2024-07-02 15:26:41 +0900

    templates 및 url path 추가

--- django_test/mysite/pybo/views.py
+++ django_test/mysite/pybo/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world")
+    q_list = Question.objects.order_by('-created_at')
+    context = {'question_list':q_list}
+    return render(request, 'pybo/question_list.html', context)
 
+
+def detail(request, question_id):
+    # q_detail = Question.objects.get(id=question_id)
+    q_detail = get_object_or_404(Question, pk=question_id)
+    context = {'question':q_detail}
+    return render(request, 'pybo/question_detail.html', context)
